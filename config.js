45e8322007cc0250954105c0cb33e6d27c7898fa
Tod <tpoirier@nmu.edu>
2026-09-09 12:39:53 -0400

    Add ancestry Supabase configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.LAZY_ACRES_ANCESTRY_CONFIG = {
+  supabaseUrl: 'https://wntakzfoprthwggkidyq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_gWu_EQ1J3s0iNjeDeINJwQ_xKy8QgAJ',
+  tablePrefix: 'lazy_acres_ancestry_',
+};

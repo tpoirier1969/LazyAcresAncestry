de4adda07c626447a6ac4b92515e5e032bf5ec0d
Tod <tpoirier@nmu.edu>
2026-09-14 10:14:44 -0400

    Use high-detail world texture for antique globe

--- src/atlas-map.js
+++ src/atlas-map.js
@@ -1,1 +1,4 @@
-export const ATLAS_TEXTURE_URL = new URL('../assets/antique-atlas-texture.svg', import.meta.url).href;
+export const ATLAS_TEXTURE_URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Equirectangular-projection-topographic-world.jpg';
+export const ATLAS_TEXTURE_SOURCE_PAGE = 'https://commons.wikimedia.org/wiki/File:Equirectangular-projection-topographic-world.jpg';
+export const ATLAS_TEXTURE_CREDIT = 'Gundan / mapswire.com, CC BY-SA 4.0';
+export const ATLAS_TEXTURE_SHA1 = '2d069905a76447a5de0c11bb02628fb8d8323528';

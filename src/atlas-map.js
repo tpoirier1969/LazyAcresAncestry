7322700e1d44659928d6e29bab8ced5c7c4a3fee
Tod <tpoirier@nmu.edu>
2026-09-14 09:17:17 -0400

    Keep atlas module as canonical texture source

--- src/atlas-map.js
+++ src/atlas-map.js
@@ -1,17 +1,1 @@
 export const ATLAS_TEXTURE_URL = new URL('../assets/antique-atlas-texture.svg', import.meta.url).href;
-
-let cachedTexture = null;
-
-export function getAtlasTexture(onReady) {
-  if (cachedTexture) {
-    if (cachedTexture.complete && cachedTexture.naturalWidth) onReady?.(cachedTexture);
-    return cachedTexture;
-  }
-
-  const image = new Image();
-  image.decoding = 'async';
-  image.src = ATLAS_TEXTURE_URL;
-  image.addEventListener('load', () => onReady?.(image), { once: true });
-  cachedTexture = image;
-  return image;
-}
